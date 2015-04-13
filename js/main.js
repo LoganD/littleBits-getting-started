be559220dee4799745c7bc13b93ae88a3593392e
Logan Donovan <commit@logandonovan.com>
2015-04-13 00:32:23 -0400

    added basic page and styling for app

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var littlebits;
+
+var cloudBit_tokens = {
+    device_id: '',
+    access_token: ''
+};
+
+function setDeviceID(id) {
+  cloudBit_tokens.device_id = id;
+}
+
+function setDeviceIDInput(){
+  if (cloudBit_tokens.device_id !== '') {
+    $('#deviceID').val(cloudBit_tokens.device_id);
+  }
+}
+
+function setAccessToken(token) {
+  cloudBit_tokens.access_token = token;
+}
+
+function setAccessTokenInput() {
+  if (cloudBit_tokens.access_token !== '') {
+    $('#access_token').val(cloudBit_tokens.access_token);
+  }
+}
+
+function resetCloudBitTokens(){
+  setDeviceID('');
+  setAccessToken('');
+}
+
+function initSettingsInputs(){
+  setDeviceIDInput();
+  setAccessTokenInput();
+}
+
+function setCloutBitTokensFromForm(){
+  cloudBit_tokens.device_id = $('#deviceID').val();
+  cloudBit_tokens.access_token = $('#access_token').val();
+}
+
+$(document).ready(function() {
+  initSettingsInputs();
+
+  $('#clearSettings').on('click', function() {
+    resetCloudBitTokens();
+    setDeviceIDInput();
+    setAccessTokenInput();
+  });
+  $('#saveSettings').on('click', setCloutBitTokensFromForm());
+});
